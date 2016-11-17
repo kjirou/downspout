c18116c805feb97423c2180746ef8cc97b322719
kjirou <sorenariblog@gmail.com>
2016-11-17 22:05:22 +0900

    Add tests

--- test/lib/UseCase.js
+++ test/lib/UseCase.js
@@ -5,12 +5,37 @@ const UseCase = require('../../lib/UseCase');
 
 describe('lib/UseCase', () => {
   describe('_promisifyResult', () => {
+    it('should return a resolved promise if undefined is passed', done => {
+      UseCase._promisifyResult(undefined)
+        .then(result => {
+          assert.strictEqual(result, undefined);
+          done();
+        })
+    });
+
     it('should return a rejected promise if a error is passed', done => {
       UseCase._promisifyResult(new Error('ERR!'))
         .catch(err => {
           assert.strictEqual(err.message, 'ERR!');
           done();
-        });
+        })
+      ;
+    });
+
+    it('should continue chaining promises if a resolved promise is passed', done => {
+      UseCase._promisifyResult(Promise.resolve(123))
+        .then(result => {
+          assert.strictEqual(result, 123);
+          done();
+        })
+    });
+
+    it('should continue chaining promises if a rejected promise is passed', done => {
+      UseCase._promisifyResult(Promise.reject(456))
+        .catch(result => {
+          assert.strictEqual(result, 456);
+          done();
+        })
     });
   });
 });
